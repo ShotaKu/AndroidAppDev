859dec216c9015c3d7cc957abd24d50b0a35f6e9
Shota <sh.kuroda0524@gmail.com>
2018-11-05 16:57:55 +0700

    [Edit] Complete WIP project week 10

--- ProjectWeek10/BtpNavJson/app/src/main/java/com/shota/android/btpnavjson/MainActivity.kt
+++ ProjectWeek10/BtpNavJson/app/src/main/java/com/shota/android/btpnavjson/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.shota.android.btpnavjson
 
-import android.content.res.AssetManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.google.gson.Gson
-import com.shota.android.btpnavjson.R.layout.customcell
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.InputStream
+import java.text.DateFormatSymbols
+import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.ArrayList
+import android.view.LayoutInflater
+import kotlinx.android.synthetic.main.customcell.view.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,16 +26,40 @@ class MainActivity : AppCompatActivity() {
 
         val gson = Gson()
         val datas = gson.fromJson(json,Array<Data>::class.java);
-        val list = list
+        val list:LinearLayout = list
         var lastMonth = 0;
         for(data:Data in datas){
-            if(data.getDate().month != lastMonth){
-
+            val dateTime = data.getDate();
+            if(dateTime.get(Calendar.MONTH) != lastMonth){
+                lastMonth = dateTime.get(Calendar.MONTH)
+                var text = TextView(this);
+                text.textSize = 18f;
+                text.setPadding(0,5,0,5)
+                if(0<=lastMonth){
+                    text.setText(DateFormatSymbols().getMonths()[dateTime.get(Calendar.MONTH)]+" "+dateTime.get(Calendar.YEAR));
+                }else{
+                    break;
+                }
+                list.addView(text);
             }
-            val cCell = View(customcell)
-            cCell.date
+            val cell = createCell(SimpleDateFormat("dd/MM/yyyy").format(dateTime.time),data.preTaxNav.toString(),data.preTaxBid.toString());
+            list.addView(cell);
         }
     }
-
+    fun createCell(date:String,data1:String,data2:String): View {
+        val factory = LayoutInflater.from(this)
+        val myView = factory.inflate(R.layout.customcell, null)
+        myView.date.setText(date);
+        myView.data1.setText(data1);
+        myView.data2.setText(data2);
+        return myView;
+    }
+    fun createText(str:String): TextView {
+        var text = TextView(this);
+        text.textSize = 18f;
+        text.setPadding(0,5,0,5)
+        text.setText(str)
+        return text;
+    }
     //data class  data(val date: Date, val preTaxNav:Double, val preTaxBid:Double, val fundSize:Double, val change:Double, val percentChange:Double)
 }
